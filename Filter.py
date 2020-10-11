c26c68d024b7d125806b2742c7eca2cd92001ad7
thijsrijkers <thijsrijkers@live.nl>
2020-10-11 16:18:05 +0200

    +Made the filter better

--- Filter.py
+++ Filter.py
@@ -1,8 +1,12 @@
 import json
 
 with open('Storage\data.json') as json_file:
-    data = json.load(json_file)    
-    print(data["WordsEntered"])
+    # convert JSON text to python dictionary
+    data = json.loads(json_file.read())
+    list = data['WordsEntered']
+    # print data
+    for data in list:
+        print(data['Word'])
 
 #[TODO] Remove this input when develop is finished
 input("Press enter to exit ;)")
