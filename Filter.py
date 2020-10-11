a68b93885cbd5d711d5990233e68544942ab5536
thijsrijkers <thijsrijkers@live.nl>
2020-10-11 22:49:24 +0200

    +Small AI made for the usage of the filter +JSON data storage created for the best topic generated

--- Filter.py
+++ Filter.py
@@ -1,12 +1,30 @@
 import json
 
+wordCount = []
+
 with open('Storage\data.json') as json_file:
-    # convert JSON text to python dictionary
     data = json.loads(json_file.read())
     list = data['WordsEntered']
-    # print data
-    for data in list:
-        print(data['Word'])
 
-#[TODO] Remove this input when develop is finished
-input("Press enter to exit ;)")
+    for data in list:
+        wordCount.append(data['Word'])
+
+def most_frequent(wordCount): 
+    return max(set(wordCount), key = wordCount.count) 
+
+def write_json(data, filename='Storage\BestTopic.json'): 
+    with open(filename,'w') as f: 
+        json.dump(data, f, indent=4) 
+
+with open('Storage\BestTopic.json') as json_file: 
+    data = json.load(json_file) 
+      
+    temp = data['BestTopic'] 
+  
+    y = {
+            "Topic": most_frequent(wordCount)
+        } 
+  
+    temp.append(y) 
+      
+write_json(data)  
